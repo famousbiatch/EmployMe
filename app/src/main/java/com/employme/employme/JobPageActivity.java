9d340af5fff982b8eedf4450d7abcc3f3997f2f9
famousbiatch <mojangfan2@gmail.com>
2018-03-27 21:53:44 +0300

    -

--- app/src/main/java/com/employme/employme/JobPageActivity.java
+++ app/src/main/java/com/employme/employme/JobPageActivity.java
@@ -2,8 +2,8 @@ package com.employme.employme;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
